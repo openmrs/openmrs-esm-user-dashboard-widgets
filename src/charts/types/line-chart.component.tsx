d6ccf548f22c930b1068c22e9c41d7d2d6556152
charles161 <fortloop.charles@gmail.com>
2020-01-03 13:22:21 +0530

    Added functionality to Chart Widget to Support Multiple Lines

--- src/charts/types/line-chart.component.tsx
+++ src/charts/types/line-chart.component.tsx
@@ -6,20 +6,34 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  ResponsiveContainer
+  ResponsiveContainer,
+  Legend
 } from "recharts";
 
 export default function(props: LineChartProperties) {
   const { data, yAxis, xAxis, lineStroke, gridStroke } = props;
 
+  function getLinesForChart() {
+    return yAxis
+      .split(";")
+      .map((label, index) => (
+        <Line
+          type="monotone"
+          dataKey={label}
+          stroke={lineStroke.split(";")[index]}
+        />
+      ));
+  }
+
   return (
     <ResponsiveContainer>
       <LineChart data={data} margin={{ top: 5, right: 5, bottom: 0, left: 0 }}>
-        <Line type="monotone" dataKey={yAxis} stroke={lineStroke} />
+        {getLinesForChart()}
         <CartesianGrid stroke={gridStroke} strokeDasharray="5 5" />
         <XAxis dataKey={xAxis} tick={{ fill: "#00463f", fontSize: "10px" }} />
-        <YAxis dataKey={yAxis} tick={{ fill: "#00463f", fontSize: "10px" }} />
+        <YAxis />
         <Tooltip />
+        <Legend iconType={"line"} height={20} width={300} iconSize={15} />
       </LineChart>
     </ResponsiveContainer>
   );
