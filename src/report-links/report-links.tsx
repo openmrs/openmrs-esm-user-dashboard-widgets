1df47fa960fdc14a2221dfd751672fa6b1648670
Chitralekha <Chitralekha.k@thoughtworks.com>
2020-01-17 13:48:28 +0530

    Created Table view for report links- WIP

--- src/report-links/report-links.tsx
+++ src/report-links/report-links.tsx
@@ -5,21 +5,46 @@ import WidgetHeader from "../commons/widget-header/widget-header.component";
 
 export default function ReportLinks(props: ReportLinksProps) {
   const getKey = (name: string) => name.replace(/ /g, "-");
+
+  const divstyle = {
+    padding: "10px",
+    "text-align": "left",
+    overflow: "scroll"
+  };
+
+  const rowStyle = {
+    borderBottomColor: "blue",
+    borderBottomWidth: 1
+  };
+
+  const tableStyle = {
+    border: "none",
+    boxShadow: "none",
+    "border-spacing": "0px"
+  };
   const reportLinkElement = (reportLink: ReportLink) => (
-    <div key={getKey(reportLink.name)}>
-      <a href={reportLink.link} target="_blank" title={reportLink.name}>
-        {reportLink.name}
-      </a>
-    </div>
+    <tr style={rowStyle}>
+      <td>
+        <i className={"icon-record"}></i>
+      </td>
+      <td key={getKey(reportLink.name)}>
+        <a href={reportLink.link} target="_blank" title={reportLink.name}>
+          {reportLink.name}
+        </a>
+      </td>
+    </tr>
   );
 
   return (
     <>
       <WidgetHeader
         title={props.title}
+        totalCount={props.reports ? props.reports.length : 0}
         icon="svg-icon icon-todo"
       ></WidgetHeader>
-      {props.reports.map(reportLinkElement)}
+      <div style={divstyle}>
+        <table style={tableStyle}>{props.reports.map(reportLinkElement)}</table>
+      </div>
     </>
   );
 }
