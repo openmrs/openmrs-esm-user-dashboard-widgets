21e3ecd6599110374fe15c5ce1a96ff5e0490b1e
Chitralekha <Chitralekha.k@thoughtworks.com>
2020-01-22 00:44:25 +0530

    Included VisitId as TODO attribute

--- src/todo/todo.component.test.tsx
+++ src/todo/todo.component.test.tsx
@@ -29,7 +29,6 @@ describe(`<Todo />`, () => {
 
   const mockTodos = [
     {
-      todoId: 1,
       markedDone: false,
       dateCreated: 1572879146000,
       patient: {
@@ -39,12 +38,10 @@ describe(`<Todo />`, () => {
       },
       attributes: [
         {
-          id: 23,
           uuid: "0458d18d-b779-4141-b1a2-a7977f49c391",
           attributeType: "Appointment",
           dataType: "Appointment",
           value: {
-            id: 97,
             uuid: "609cd7a8-84a0-42a0-a7ec-13551acbd928",
             date: 1576132200000,
             service: {
@@ -61,32 +58,32 @@ describe(`<Todo />`, () => {
       uuid: "1ab4a2c8-1cf5-46d8-8d7a-3be35c4d8b75"
     },
     {
-      todoId: 2,
       markedDone: true,
       dateCreated: 1572584857000,
       patient: {
         name: "Patient Two",
         identifier: "103450X",
-        id: 14,
         uuid: "6b42e575-2e44-40c1-a14c-8637cbb3d21f"
       },
       attributes: [
         {
-          id: 1,
           uuid: "dcfb79f5-e443-483a-92c5-72942e9f581e",
           attributeType: "Service Category",
           dataType: "Concept",
           value: {
-            id: 166290,
             name: "Prostheses",
             uuid: "84b2d351-393a-4cff-8c3c-f5785c05f9ce"
           }
+        },
+        {
+          uuid: "90a45c11-0829-40e2-97d9-b51deea4831d",
+          attributeType: "VisitID",
+          dataType: "VisitID",
+          value: "3479bf97-b842-4f94-a424-253297e4dc5e"
         }
       ],
       type: "PRINT_CONSENT",
-      encounterId: 37,
-      uuid: "da6d1694-2eda-4ada-944e-10ccac16c1d4",
-      visitUUId: "3479bf97-b842-4f94-a424-253297e4dc5e"
+      uuid: "da6d1694-2eda-4ada-944e-10ccac16c1d4"
     }
   ];
 
